5a830d55ccf88a6ed804d2b016e2b4d36db0abc5
mx <1790405816@qq.com>
2023-11-13 13:22:20 +0800

    2023.11.13的第二次尝试git

---
+++ first.py
@@ -0,0 +1,26 @@
+
+def solve_n_queens(n):
+    def is_valid(board, row, col):
+        for i in range(row):
+            if board[i] == col or \
+               board[i] - i == col - row or \
+               board[i] + i == col + row:
+                return False
+        return True
+
+    def backtrack(board, row):
+        if row == n:
+            result.append(board[:])
+            return
+        for col in range(n):
+            if is_valid(board, row, col):
+                board[row] = col
+                backtrack(board, row + 1)
+
+    result = []
+    board = [-1] * n
+    backtrack(board, 0)
+    return result
+
+n = 4
+print(solve_n_queens(n))
